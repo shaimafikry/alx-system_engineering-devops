54562bccf28d6d05531330e8e7e53ff36a01c67f
shaimaa <shaimafikry@yahoo.com>
2024-03-25 02:24:58 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
 # Using Puppet, install flask from pip3
 
 exec { 'pip3_update':
-  command => '/usr/bin/pip3 update',
+  command => '/usr/bin/pip3 update'
 }
 
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
-  require => Exec['pip3_update'],
+  require => Exec['pip3_update']
 }
