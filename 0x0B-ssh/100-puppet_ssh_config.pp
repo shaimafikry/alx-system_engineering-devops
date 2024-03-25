26d35c1bbcbd08fcc95dce65c1082772a41d384b
shaimaa <shaimafikry@yahoo.com>
2024-03-25 04:07:42 +0200

    edit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
-Let’s practice using Puppet to make changes to our configuration file. Just as in the previous configuration file task, we’d like you to set up your client SSH configuration file so that you can connect to a server without typing a password.
-
-Requirements:
+# using Puppet to make changes to our configuration file.
+# Just as in the previous configuration file task
+# we’d like you to set up your client SSH configuration file
+# so that you can connect to a server without typing a password.
 
 file {'edit_config_file':
   ensure => 'present'
