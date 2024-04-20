a28356bdfa33737e2134d6f556c2ce9ed3e08996
shaimaa <shaimafikry@yahoo.com>
2024-04-20 21:21:36 +0200

    add ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 # Just as in the previous configuration file task
 # we’d like you to set up your client SSH configuration file
 # so that you can connect to a server without typing a password.
-
+# (edit_config_file) is the name of process
 file {'edit_config_file':
   ensure  => 'present'
   path    => '/etc/ssh/ssh_config'
