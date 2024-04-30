f45c429a76b999f7278ed144e952b8ee6f89ddf1
shaimaa <shaimafikry@yahoo.com>
2024-05-01 00:43:04 +0300

    edit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,19 +3,10 @@ package {'nginx':
   ensure  => 'installed',
 }
 
-file { '/etc/nginx/sites-available/default':
+file_line { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        add_header X-Served-By $(hostname);
-        location / {
-                try_files /$(uri) $(uri) =404;
-        }
-}",
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;'
   require => Package['nginx']
 }
 # to applaya restart
