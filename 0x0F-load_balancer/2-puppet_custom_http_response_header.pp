ff8b3b88b55df98f302211c7f9b2a8757d5780a9
shaimaa <shaimafikry@yahoo.com>
2024-04-30 03:10:59 +0300

    edit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,36 @@
 # custom http request header
+# use puppet to configure the server
+package {'nginx':
+  ensure  => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Hello World!\n",
+  require => Package['nginx']
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        # applyin redirect
+        location /redirect_me {
+                return 301;
+        }
+        location / {
+             root /var/www/html;
+             add_header X-Served-By $(hostname);
+        }
+        }',
+  require => Package['nginx']
+}
+# to applaya restart
+service { 'nginx':
+   ensure => running,
+   enable => true,
+}
