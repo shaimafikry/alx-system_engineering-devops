f51e957bd91d3e20c79b74e74b69c0ee28ff4bbc
shaimaa <shaimafikry@yahoo.com>
2024-05-01 01:12:03 +0300

    edit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ file { '/etc/nginx/sites-available/default':
         server_name _;
         add_header X-Served-By ${hostname};
         location / {
-                try_files / =404;
+               root /var/www/html;
         }
 }",
   require => Package['nginx']
