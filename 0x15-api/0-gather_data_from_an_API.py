9a0d561611420874f123a2f7b2eb1e607e08aa10
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-05-24 02:19:38 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,38 @@
 #!/usr/bin/python3
 # Libraries imported in your Python files must be organized in alphabetical order
 if __name__ == "__main__":
+import sys
+import requests
+
+if len(sys.argv) > 1:
+    id = sys.argv[1]
+    u_tasks = []
+    comp_tasks = []
+    rest_api = "https://jsonplaceholder.typicode.com"
+    # add .json to bring the json format as dict
+    # if not used output would be [response 200]
+    data = requests.get(f"{rest_api}/users/{id}").json()
+    # print (data)
+    name = data["name"]
+    # print(name)
+    tasks = requests.get(f"{rest_api}/todos/").json()
+    for i in tasks:
+    if i["userId"] == id:
+        u_tasks.append(i)
+    # print (tasks)
+    # print (u_tasks)
+    total_u_tasks = len(u_tasks)
+    for i in u_tasks:
+        if i["completed"] == True:
+            comp_tasks.append(i)
+    # print (comp_tasks)
+    total_comp_tasks = len(comp_tasks)
+    # print (total_comp_tasks)
+    print(f"Employee {name} is done with tasks({total_comp_tasks}/{total_u_tasks}):")
+    for i in comp_tasks:
+        task_name = i["title"]
+        print("\t {}".format(task_name))
+else:
+    print("Usage: missing id")
+        
     
