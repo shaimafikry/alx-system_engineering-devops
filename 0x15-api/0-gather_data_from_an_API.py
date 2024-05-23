342a6fa06bcdba85e83cb3abd9a50dbb74365a1d
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-05-24 02:21:48 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Libraries imported in your Python files must be organized in alphabetical order """
+"""
+Libraries imported in your Python files must be organized in alphabetical order
+"""
 
 
 if __name__ == "__main__":
