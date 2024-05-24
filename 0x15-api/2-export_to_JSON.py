bd62266d5e31476c4452c18ee90bffae480f721a
shaimaa <shaimafikry@yahoo.com>
2024-05-24 17:33:36 +0300

    edit

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,27 +9,31 @@ import sys
 import json
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        id = sys.argv[1]
+        id = int(sys.argv[1])
         u_tasks = []
         comp_tasks = []
-        filename = "USER_ID.json"
+        filename = "{}.json".format(id)
+        employ_dict = {}
         rest_api = "https://jsonplaceholder.typicode.com"
         # add .json to bring the json format as dict
         # if not used output would be [response 200]
         data = requests.get(f"{rest_api}/users/{id}").json()
         # print (data)
-        name = data["name"]
+        name = data["username"]
         # print(name)
         tasks = requests.get(f"{rest_api}/todos/").json()
         for i in tasks:
-        if i["userId"] == id:
-            u_tasks.append(i)
-        # print (tasks)
+            if i["userId"] == id:
+                u_tasks.append(i)
         # print (u_tasks)
-        total_u_tasks = len(u_tasks)
-        fields = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE]
-        with open (filename,"w") as file:
-            data = json.dumps(u_tasks)
+        for i in u_tasks:
+            i.pop("userId")
+            i.pop("id")
+            i["username"] = name
+        employ_dict[id] = u_tasks
+        # print (employ_dict)
+        with open(filename, "w") as file:
+            data = json.dumps(employ_dict)
             file.write(data)
     else:
-        print("Usage: missing id")   
+        print("Usage: missing id")
