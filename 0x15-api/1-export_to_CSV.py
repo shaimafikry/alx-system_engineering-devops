4e7c4ddeecae448f30923783ec5e6f678e5e01b2
shaimaa <shaimafikry@yahoo.com>
2024-05-24 17:11:08 +0300

    edit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         id = int(sys.argv[1])
         u_tasks = []
-        comp_tasks = []
         filename = "USER_ID.csv"
         rest_api = "https://jsonplaceholder.typicode.com"
         # add .json to bring the json format as dict
@@ -25,9 +24,7 @@ if __name__ == "__main__":
             if i["userId"] == id:
                 i["name"] = name
                 u_tasks.append(i)
-        # print (tasks)
         # print (u_tasks)
-        total_u_tasks = len(u_tasks)
         fields = ["userId", "name", "completed", "title"]
         with open(filename, "w", newline='') as csvfile:
             data = csv.DictWriter(csvfile, fieldnames=fields)
