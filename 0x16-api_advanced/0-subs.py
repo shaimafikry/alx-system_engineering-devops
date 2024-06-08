9b4535001fb2ded9d453ef188fac5b800a57ce0e
shaimaa <shaimafikry@yahoo.com>
2024-06-08 13:49:54 +0300

    first task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# func to get the num of suscribers of a given subreddit
+import requests
+
+
+def number_of_subscribers(subreddit):
+    # end point url
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    data = requests.get(url)
+    if (data.status_code == 200):
+        content = data.json()
+        extract_content = content["data"]
+        num = extract_content.get("subscribers")
+        # print (num)
+        return num
+    else:
+        return 0
