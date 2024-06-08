e6e33e58534118f10ff7fc5cd464b586e9a0950d
shaimaa <shaimafikry@yahoo.com>
2024-06-08 15:15:54 +0300

    first task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """func to get the num of suscribers of a given subreddit"""
     # end point url
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "shaimaa"}
+    headers = {"User-Agent": "Myscript/1.0 (0-subs)"}
     data = requests.get(url, headers=headers)
     if (data.status_code == 200):
         content = data.json()
