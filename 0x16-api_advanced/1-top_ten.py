65aa65e9b97394127aa95f4fbf23cf38d0ba1efd
shaimaa <shaimafikry@yahoo.com>
2024-06-08 16:29:13 +0300

    first task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,13 @@ def top_ten(subreddit):
     """func to get the top 10 comments"""
     # end point url
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"limit": 10}
     headers = {"User-Agent": "Myscript/1.0 (0-subs)"}
-    data = requests.get(url, headers=headers)
+    data = requests.get(url, headers=headers, params=params)
     if (data.status_code == 200):
         content = data.json()
         comments = content["data"]["children"]
-        i = 1
         for comment in comments:
             print(comment["data"]["title"])
-            i = i + 1
-            if i == 10:
-                return
     else:
         return None
