80f05efef1c1d3f0661304144c869ff8fa85bc37
shaimaa <shaimafikry@yahoo.com>
2024-06-08 14:06:06 +0300

    first task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ def number_of_subscribers(subreddit):
     """func to get the num of suscribers of a given subreddit"""
     # end point url
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    data = requests.get(url)
+    headers = {"User-Agent": "your_custom_user_agent"}
+    data = requests.get(url, headers=headers)
     if (data.status_code == 200):
         content = data.json()
-        extract_content = content["data"]
-        num = extract_content.get("subscribers")
+        num = content["data"]["subscribers"]
         # print (num)
         return num
     else:
